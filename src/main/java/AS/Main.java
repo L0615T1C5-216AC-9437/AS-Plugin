4afa59de5ce4eb9ce96655c5a3c6c98a3fa62e1e
L0615T1C5-216AC-9437 <60281819+L0615T1C5-216AC-9437@users.noreply.github.com>
2020-02-25 21:11:15 -0500

    Genesis

---
+++ src/main/java/AS/Main.java
@@ -0,0 +1,55 @@
+package AS;
+
+//-----imports-----//
+import arc.Events;
+import arc.util.Log;
+import mindustry.entities.type.Player;
+import mindustry.game.EventType;
+import mindustry.gen.Call;
+import mindustry.plugin.Plugin;
+
+import java.util.concurrent.TimeUnit;
+
+import static mindustry.Vars.*;
+
+
+public class Main extends Plugin {
+    public boolean ass = false; // ass -> auto sync started, not THAT
+
+    public Main() throws InterruptedException {
+        Events.on(EventType.WaveEvent.class, event -> {
+            if(!ass) {
+                Thread AS = new Thread() {
+                    public void run() {
+                        Log.info("AS started Successfully!");
+                        while (true) {
+                            for (Player p : playerGroup.all()) {
+                                Call.onWorldDataBegin(p.con);
+                                netServer.sendWorldData(p);
+                                Call.onInfoToast(p.con, "Auto Sync completed.", 5);
+                                try {
+                                    TimeUnit.SECONDS.sleep(1);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            try {
+                                TimeUnit.SECONDS.sleep(4 * 60);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            Call.onInfoToast("Auto Sync in 1 minute.", 10);
+                            try {
+                                TimeUnit.SECONDS.sleep(60);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                };
+                AS.start();
+                Log.info("Attempting to start AS...");
+            }
+        });
+    }
+}
